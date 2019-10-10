e86e064d99f8cd02a4ab06c7265750e745641106
amanda.santos <amanda.santos@sqginfo.com.br>
2019-10-10 16:08:54 -0300

    Mudanca de versao para 94. Correça na conta do fechamento de mesa para truncar o valor da taxa de servico depois de somar para ficar igual no PDV Restaurante e no Terminal Balcao

--- www/assets/js/controllers/ContaController.js
+++ www/assets/js/controllers/ContaController.js
@@ -1,37 +1,37 @@
 ﻿angular.module('app').controller('ContaController', ['$rootScope', '$scope', 'produtos', 'pedido', 'config', 'ws', 'funcoes', 'toast', '$location', '$routeParams', '$timeout', 'atendimento', 'operador', ContaController]);
 
-function ContaController($rootScope, $scope, produtos, pedido, config, ws, funcoes, toast, $location, $routeParams, $timeout, atendimento, operador){
+function ContaController($rootScope, $scope, produtos, pedido, config, ws, funcoes, toast, $location, $routeParams, $timeout, atendimento, operador) {
     var vm = this;
     vm.mesa = pedido.getMesa();
     vm.comanda = pedido.getComanda();
     vm.operador = operador.getOperador();
     vm.terminal = config.ip;
-	vm.valorServico = 0;
+    vm.valorServico = 0;
     vm.configuracao = config.configuracao;
-	vm.microterminal = config.microterminal;
-	vm.date = new Date();
-	vm.tipoAtendimento = config.tipoAtendimento;
-    vm.getTotalComanda = function(itens){
-    	return TotalComanda(itens);
+    vm.microterminal = config.microterminal;
+    vm.date = new Date();
+    vm.tipoAtendimento = config.tipoAtendimento;
+    vm.getTotalComanda = function (itens) {
+        return TotalComanda(itens);
     };
 
-    vm.emitirConta = function(){
+    vm.emitirConta = function () {
         var arrayDeComandas = funcoes.MapComandas(vm.comandas);
-        var stringComandas = funcoes.ArrayToString(arrayDeComandas,true);
+        var stringComandas = funcoes.ArrayToString(arrayDeComandas, true);
         pedido.emitirConta(vm.mesa, stringComandas, operador.getOperador().idOperador, config.ip)
-        .then(
-            function(data){
-                var json = angular.fromJson($(data).find('EmitirContaResult').text());
-                toast.showCustomToast(json.Mensagem);
-                if (json.FlagMensagem) {
-                    atendimento.getUrlRetornoComanda();
-                    $timeout(function(){$rootScope.$apply();});
+            .then(
+                function (data) {
+                    var json = angular.fromJson($(data).find('EmitirContaResult').text());
+                    toast.showCustomToast(json.Mensagem);
+                    if (json.FlagMensagem) {
+                        atendimento.getUrlRetornoComanda();
+                        $timeout(function () { $rootScope.$apply(); });
+                    }
+                },
+                function (error) {
+                    console.log(error);
                 }
-            }, 
-            function(error){
-                console.log(error);
-            }
-        );
+            );
     };
     //Se veio a comanda, é emissão de conta por comanda;
     if ($routeParams.comanda) {
@@ -41,19 +41,19 @@ function ContaController($rootScope, $scope, produtos, pedido, config, ws, funco
         vm.comanda = 0;
     }
 
-	
-    ws.ObterMesaComanda(vm.mesa,vm.comanda,vm.operador.idOperador, false).then(
-    	function(data){
-    		var json = angular.fromJson($(data).find('ObterMesaComandaResult').text());
-    		vm.comandas = json;
-			
+
+    ws.ObterMesaComanda(vm.mesa, vm.comanda, vm.operador.idOperador, false).then(
+        function (data) {
+            var json = angular.fromJson($(data).find('ObterMesaComandaResult').text());
+            vm.comandas = json;
+
             var comandasSelecionadas = [];
-            if($routeParams.comandas){
+            if ($routeParams.comandas) {
                 comandasSelecionadas = angular.fromJson($routeParams.comandas);
-                vm.comandas = _.filter(vm.comandas,function(item){
+                vm.comandas = _.filter(vm.comandas, function (item) {
                     var retornar = false;
-                    _.each(comandasSelecionadas, function(numComanda){
-                        if(numComanda == item.idComanda){
+                    _.each(comandasSelecionadas, function (numComanda) {
+                        if (numComanda == item.idComanda) {
                             retornar = true;
                         }
                     });
@@ -61,142 +61,146 @@ function ContaController($rootScope, $scope, produtos, pedido, config, ws, funco
                 });
             }
 
-            _.each(vm.comandas,function(item){
+            _.each(vm.comandas, function (item) {
                 item.liAtendimentoItem = funcoes.MapProdutos(item.liAtendimentoItem);
-				
-            });
-          
-    		if(vm.comandas.length > 1){
-    			vm.pessoas= vm.comandas.length;
-    		}else{
-    			vm.pessoas = vm.comandas[0].nrPessoas;
-    		}
-			
-			_.each(vm.comandas, function(comanda){
-				var itens = [];
-				_.each(comanda.liAtendimentoItem,function(item){
-					if(vm.configuracao.flFechamentoAnalitico){
-						if(item.flFracionado){
-							itens.push(item);
-						}else{
-							for(var qt = 0; qt < item.nrQuantidade; qt++){
-								var clone = _.clone(item);
-								clone.nrQuantidade = 1;
-								clone.vrTotal = item.vrUnitario;
-								if(qt == (item.nrQuantidade - 1)){
-									_.each(clone.adicionais, function(adicional){
-										adicional.vrTotal = adicional.vrUnitario;
-									});
-									_.each(clone.fracionados, function(fracionado){
-										fracionado.vrTotal = fracionado.vrUnitario;
-									});
-								}else{
-									clone.adicionais = [];
-									clone.fracionados = [];
-								}
-								itens.push(clone);
-							}
-						}
-					}else{
-						var flagExiste = false;
-						var itemLocalizado = _.find(itens,function(i){
-							return i.idProduto == item.idProduto;
-						});
-						if(itemLocalizado){
-							itemLocalizado.nrQuantidade += item.nrQuantidade;
-							itemLocalizado.vrTotal += item.vrTotal;
-							SomaAdicionais(itemLocalizado, item);
-						}
-						else{
-							itens.push(item);
-						}
-					}
-				});
-				comanda.liAtendimentoItem = itens;
-			});
-			
-    		vm.subTotal = TotalMesa(vm.comandas);
-     		vm.valPessoa = vm.getTotalGeral(vm.comandas) / vm.pessoas;
-			vm.stNomeFantasia = config.configuracao.stNomeFantasia;
-     		$scope.$apply();
-    	},
-    	function(error){
 
-    	}
+            });
+
+            if (vm.comandas.length > 1) {
+                vm.pessoas = vm.comandas.length;
+            } else {
+                vm.pessoas = vm.comandas[0].nrPessoas;
+            }
+
+            _.each(vm.comandas, function (comanda) {
+                var itens = [];
+                _.each(comanda.liAtendimentoItem, function (item) {
+                    if (vm.configuracao.flFechamentoAnalitico) {
+                        if (item.flFracionado) {
+                            itens.push(item);
+                        } else {
+                            for (var qt = 0; qt < item.nrQuantidade; qt++) {
+                                var clone = _.clone(item);
+                                clone.nrQuantidade = 1;
+                                clone.vrTotal = item.vrUnitario;
+                                if (qt == (item.nrQuantidade - 1)) {
+                                    _.each(clone.adicionais, function (adicional) {
+                                        adicional.vrTotal = adicional.vrUnitario;
+                                    });
+                                    _.each(clone.fracionados, function (fracionado) {
+                                        fracionado.vrTotal = fracionado.vrUnitario;
+                                    });
+                                } else {
+                                    clone.adicionais = [];
+                                    clone.fracionados = [];
+                                }
+                                itens.push(clone);
+                            }
+                        }
+                    } else {
+                        var flagExiste = false;
+                        var itemLocalizado = _.find(itens, function (i) {
+                            return i.idProduto == item.idProduto;
+                        });
+                        if (itemLocalizado) {
+                            itemLocalizado.nrQuantidade += item.nrQuantidade;
+                            itemLocalizado.vrTotal += item.vrTotal;
+                            SomaAdicionais(itemLocalizado, item);
+                        }
+                        else {
+                            itens.push(item);
+                        }
+                    }
+                });
+                comanda.liAtendimentoItem = itens;
+            });
+
+            vm.subTotal = TotalMesa(vm.comandas);
+            vm.valPessoa = vm.getTotalGeral(vm.comandas) / vm.pessoas;
+            vm.stNomeFantasia = config.configuracao.stNomeFantasia;
+            $scope.$apply();
+        },
+        function (error) {
+
+        }
     );
 
-	function SomaAdicionais(itemLocalizado, novoItem)
-	{
-		_.each(novoItem.adicionais,function(adicionalNovo){
-			var localizado = _.find(itemLocalizado.adicionais,function(adicionalLocalizado){
-				return adicionalNovo.idProduto == adicionalLocalizado.idProduto;
-			});
-			if(localizado){
-				localizado.vrTotal += adicionalNovo.vrTotal;
-			}else{
-				itemLocalizado.adicionais.push(adicionalNovo);
-			}
-		});
-	}
-	
-    function TotalMesa (comandas){
-    	var total = 0;
-    	_.each(comandas,function(comanda){
-    		total += TotalComanda(comanda.liAtendimentoItem);
-    	});
-
-    	return total;
+    function SomaAdicionais(itemLocalizado, novoItem) {
+        _.each(novoItem.adicionais, function (adicionalNovo) {
+            var localizado = _.find(itemLocalizado.adicionais, function (adicionalLocalizado) {
+                return adicionalNovo.idProduto == adicionalLocalizado.idProduto;
+            });
+            if (localizado) {
+                localizado.vrTotal += adicionalNovo.vrTotal;
+            } else {
+                itemLocalizado.adicionais.push(adicionalNovo);
+            }
+        });
     }
 
-    function TotalComanda(itens){
-    	var total = 0;
-    	_.each(itens,function(item){
-   			total += item.vrTotal;
-			if(item.adicionais)
-			{
-				if(item.adicionais.length > 0){
-					_.each(item.adicionais, function(itemAdicional){
-						total += itemAdicional.vrTotal;
-					});
-				}
-			}
-   		});
-    	return total;
+    function TotalMesa(comandas) {
+        var total = 0;
+        _.each(comandas, function (comanda) {
+            total += TotalComanda(comanda.liAtendimentoItem);
+        });
+
+        return total;
     }
-	
-	function TotalComTaxa(comandas){
-		var vrTotalItensComTaxa = 0;
-		_.each(comandas, function(comanda){
-			_.each(comanda.liAtendimentoItem,function(item){
-				if(item.flTaxaServico)
-					vrTotalItensComTaxa += item.vrTotal;
-			});
-		});
-		return vrTotalItensComTaxa;
-	}
-	
-    vm.getTaxaServicoTotal = function (comandas) {
-        var totalTaxa = 0;
+
+    function TotalComanda(itens) {
+        var total = 0;
+        _.each(itens, function (item) {
+            total += item.vrTotal;
+            if (item.adicionais) {
+                if (item.adicionais.length > 0) {
+                    _.each(item.adicionais, function (itemAdicional) {
+                        total += itemAdicional.vrTotal;
+                    });
+                }
+            }
+        });
+        return total;
+    }
+
+    function TotalComTaxa(comandas) {
+        var vrTotalItensComTaxa = 0;
         _.each(comandas, function (comanda) {
             _.each(comanda.liAtendimentoItem, function (item) {
                 if (item.flTaxaServico)
-                    totalTaxa += vm.truncateNumber(item.vrTotal * vm.configuracao.vrTaxaServico);
+                    vrTotalItensComTaxa += item.vrTotal;
             });
         });
-    
-		//var totalComTaxa = TotalComTaxa(comandas);
-		//totalComTaxa = totalComTaxa * vm.configuracao.vrTaxaServico;
+        return vrTotalItensComTaxa;
+    }
+
+    vm.getTaxaServicoTotal = function (comandas) {
+        var totalTaxa = 0;
+        var totalParcial = 0;
+        _.each(comandas, function (comanda) {
+            _.each(comanda.liAtendimentoItem, function (item) {
+                if (item.flTaxaServico)
+                    totalParcial += item.vrTotal * vm.configuracao.vrTaxaServico
+            });
+            totalTaxa = vm.truncateDecimals(totalParcial);
+        });
+
+        //var totalComTaxa = TotalComTaxa(comandas);
+        //totalComTaxa = totalComTaxa * vm.configuracao.vrTaxaServico;
         return totalTaxa;
-	};
-	
-	vm.getTotalGeral = function (comandas){
-		if(vm.microterminal.fltaxaServico)
-			return TotalMesa(comandas) + vm.getTaxaServicoTotal(comandas);
-		else
-			return TotalMesa(comandas);
     };
 
-    vm.truncateNumber = function (value){
-       return Math.floor(value * 100) / 100;
+    vm.getTotalGeral = function (comandas) {
+        if (vm.microterminal.fltaxaServico)
+            return TotalMesa(comandas) + vm.getTaxaServicoTotal(comandas);
+        else
+            return TotalMesa(comandas);
+    };
+
+    vm.truncateNumber = function (value) {
+        return Math.floor(value * 100) / 100;
+    }
+
+    vm.truncateDecimals = function (value) {
+        return Math.trunc(value * 100) / 100;
     }
 }
